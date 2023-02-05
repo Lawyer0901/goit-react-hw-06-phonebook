9e9917e132f02399e18d03f48c0055247c177d81
Lawyer0901 <105603303+Lawyer0901@users.noreply.github.com>
2023-02-05 11:23:52 +0200

    react and persisit in index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { App } from 'components/App';
 import { theme } from './components/theme';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter basename="/goit-react-hw-06-phonebook">
+      <ThemeProvider theme={theme}>
+        <PersistGate>
+          <App />
+        </PersistGate>
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
