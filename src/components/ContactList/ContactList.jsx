64221a82375706c364572ac44fbcd89a7979b866
Lawyer0901 <lawyer0901@gmail.com>
2023-02-07 12:02:11 +0200

    fixed filter

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,18 +2,30 @@
 // import { Text } from './ContactList.styled';
 import { ContactListItem } from './ContactListItem/ContactListItem';
 import { useSelector } from 'react-redux';
-import { selectUsers } from 'redux/usersSelector';
+import { selectFilter, selectUsers } from 'redux/usersSelector';
 import { useDispatch } from 'react-redux';
 import { deleteUser } from 'redux/userNameSlice';
+import { useMemo } from 'react';
 
 export function ContactList() {
   const users = useSelector(selectUsers);
+  const filter = useSelector(selectFilter);
   const dispatch = useDispatch();
+  const filterNormalize = filter => filter.toLowerCase();
+
+  const contactListToDisplay = useMemo(
+    () =>
+      users.filter(({ name }) =>
+        name.toLowerCase().includes(filterNormalize(filter))
+      ),
+    [users, filter]
+  );
+
   console.log(dispatch);
   console.log(users);
   return users.length > 0 ? (
     <ul>
-      {users.map(el => {
+      {contactListToDisplay.map(el => {
         return (
           <ContactListItem
             key={el.id}
